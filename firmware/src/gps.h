a4ecfe7502c981dc85e6e66fe29a55dba53459ab
Ryan Jacobs <ryan@rmj.us>
2018-03-31 17:48:58 -0700

    add gps.h

---
+++ firmware/src/gps.h
@@ -0,0 +1,10 @@
+#ifndef GPS_H
+#define GPS_H
+
+int direction(double lat1, double lon1, double lat2, double lon2) {
+    // do some math
+    // return value between 0 and 360
+    return 45;
+}
+
+#endif
