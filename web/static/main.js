8e59a82b5032175497d8ed9b9917b73117bbd9a0
Ryan Jacobs <ryan@rmj.us>
2018-03-31 23:13:58 -0700

    fuck

--- web/static/main.js
+++ web/static/main.js
@@ -4,9 +4,16 @@ submit.onclick = function() {
     const query = document.getElementById("query").value;
     console.log(query);
 
-    fetch("http://localhost:4848/get?query="+query)
+    fetch("http://localhost:4848/get", {
+        method: "POST",
+        body: JSON.stringify({query})
+    })
       .then(res => res.text())
       .then(text => {
-          console.log(text);
+          const venues = JSON.parse(text);
+          console.log(venues);
+
+          const div = document.getElementById("data");
+          div.innerHTML = "You are going to...." + venues[0].name;
       });
 }
